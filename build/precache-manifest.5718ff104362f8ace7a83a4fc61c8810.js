64464a6893dcd0904938bd5c3307fdf26cf58608
wb-lb443196 <wb-lb443196@alibaba-inc.com>
2021-03-19 11:36:03 +0800

    feat: 增加预览

---
+++ build/precache-manifest.5718ff104362f8ace7a83a4fc61c8810.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8904c19f6b8653a9ccb158dc25d57ab2",
+    "url": "./build/index.html"
+  },
+  {
+    "revision": "6a587316f55a172fbc47",
+    "url": "./build/static/css/2.07b2ff9d.chunk.css"
+  },
+  {
+    "revision": "c40b8e23359e416e616b",
+    "url": "./build/static/css/main.24097035.chunk.css"
+  },
+  {
+    "revision": "6a587316f55a172fbc47",
+    "url": "./build/static/js/2.8a0accac.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "./build/static/js/2.8a0accac.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c40b8e23359e416e616b",
+    "url": "./build/static/js/main.960428b4.chunk.js"
+  },
+  {
+    "revision": "2abc007b4628118d9b85",
+    "url": "./build/static/js/runtime-main.88c1953d.js"
+  }
+]);
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.f85ad0e68d99cb5a6fdb0a631d8551fb.js"
+  "./build/precache-manifest.5718ff104362f8ace7a83a4fc61c8810.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./build/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
