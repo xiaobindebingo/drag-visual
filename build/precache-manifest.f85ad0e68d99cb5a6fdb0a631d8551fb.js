bd24a466c9d407726ea5e97bf55f668286e696ac
wb-lb443196 <wb-lb443196@alibaba-inc.com>
2021-03-19 11:33:49 +0800

    feat: 增加预览

---
+++ build/precache-manifest.f85ad0e68d99cb5a6fdb0a631d8551fb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4455a9cacd22248f20f2f8f6ff1dbd26",
+    "url": "./index.html"
+  },
+  {
+    "revision": "f873e523dd0b912b23d9",
+    "url": "./static/css/2.07b2ff9d.chunk.css"
+  },
+  {
+    "revision": "c40b8e23359e416e616b",
+    "url": "./static/css/main.24097035.chunk.css"
+  },
+  {
+    "revision": "f873e523dd0b912b23d9",
+    "url": "./static/js/2.16fc953d.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "./static/js/2.16fc953d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c40b8e23359e416e616b",
+    "url": "./static/js/main.960428b4.chunk.js"
+  },
+  {
+    "revision": "ca8051aa2fe5b3ef84c3",
+    "url": "./static/js/runtime-main.673723db.js"
+  }
+]);
